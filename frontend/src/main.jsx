db6fcc5559efbc7353fe00ea57f0b81581427408
mahendra <pvdmahendra@gmail.com>
2024-12-06 09:03:32 +0530

    updated router dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,18 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'react-toastify/dist/ReactToastify.css';
+import {BrowserRouter} from 'react-router-dom'
+// import { AuthContextProvider } from './context/AuthContext.jsx'
 
-const root = createRoot(document.getElementById('root'));
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <BrowserRouter>
+       <App />
+   </BrowserRouter>
+  </React.StrictMode>,
+)
 
