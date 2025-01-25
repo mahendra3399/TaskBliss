32c1d521c028d1c6c9a718a3db33ac32bf75f668
mahendra <pvdmahendra@gmail.com>
2025-01-26 03:21:22 +0530

    remove production check for static file serving

--- backend/index.js
+++ backend/index.js
@@ -24,9 +24,8 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRouter);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-}
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
