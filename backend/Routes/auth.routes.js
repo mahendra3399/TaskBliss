9c3b012e796ebfc546228658956324dc3494610f
mahendra <pvdmahendra@gmail.com>
2025-01-26 04:10:16 +0530

    debugging issue

--- backend/Routes/auth.routes.js
+++ backend/Routes/auth.routes.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { login, logout, signup } from "../controllers/AuthController.js";
 import { loginValidation, signupValidation } from "../Middlewares/AuthValidation.js";
+import path from "path";
+
+console.log("Resolved path:", path.resolve("../controllers/AuthController.js"));
+
 
 const router = express.Router();
 
